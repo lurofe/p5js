4efd5875e106e4f649ed110a995e1058b8722036
lurofe <162892839+lurofe@users.noreply.github.com>
2024-05-31 11:37:01 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    
+  }
+  
+  function draw (){
+    stroke("blue");
+    fill("red");
+    
+    if(mouseIsPressed) {
+  rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
